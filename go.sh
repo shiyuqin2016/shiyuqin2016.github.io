f7bcd19edd9cd8d344fc8771377c558d4a8e35b6
shiyuqin2016 <982071965@qq.com>
2016-04-12 18:20:14 +0800

    ben

---
+++ go.sh
@@ -0,0 +1,8 @@
+git config --global user.email "982071965@qq.com"
+git config --global user.name "shiyuqin2016"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://shiyuqin2016.github.io
+
